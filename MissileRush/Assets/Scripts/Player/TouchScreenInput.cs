3b71d54048f57b75c6c2364091bdc3aeea3a49b7
ezowolf <wiggler@live.nl>
2016-05-04 10:10:49 +0200

    Working left/right touchscreen input

--- MissileRush/Assets/Scripts/Player/TouchScreenInput.cs
+++ MissileRush/Assets/Scripts/Player/TouchScreenInput.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
-using System.Collections;
+public class TouchScreenInput : MonoBehaviour 
+{
+	private float touchXPosition; 
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			//Checks to see if the player inputs something on the left or right side of the screen.
+			touchXPosition = Input.mousePosition.x;
+			if (touchXPosition < Screen.width / 2) {
+				print ("Mouse is on left side of screen.");
+			}
 
-public class TouchScreenInput : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+			if (touchXPosition > Screen.width / 2) {
+				print ("Mouse is on right side of screen.");
+			}
+		}
 	}
 }
